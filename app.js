8fa578c1411e31d26114f250c75e3185399b008a
18238 <1823843499@qq.com>
2025-09-26 11:41:45 +0800

    未共享版

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 App({
-  onLaunch() {
-    // Initialize app if needed
-  }
+    onLaunch() {
+        if (!wx.cloud) {
+            console.error('请使用 2.2.3 或以上的基础库以使用云能力');
+        } else {
+            wx.cloud.init({
+                env: 'cloudbase-5gkhtbhua49b94c2',  // 替换为你的环境ID
+                traceUser: true,
+            });
+        }
+        this.globalData = {};
+    }
 });
-
